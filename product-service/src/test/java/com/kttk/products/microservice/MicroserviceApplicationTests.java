dfe6bc4aac3f008b0d39de7f798ea73e360dba4b
hieuduy1751 <hieuduy1751@gmail.com>
2022-11-10 20:19:19 +0700

    add: init product microservice

---
+++ product-service/src/test/java/com/kttk/products/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kttk.products.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
