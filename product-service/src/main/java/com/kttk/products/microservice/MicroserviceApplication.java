cddd838d4239e8afdac18c2ab6affee431e3b0bc
hieuduy1751 <hieuduy1751@gmail.com>
2022-11-13 20:19:19 +0700

    add: product microservice project

--- product-service/src/main/java/com/kttk/products/microservice/MicroserviceApplication.java
+++ product-service/src/main/java/com/kttk/products/microservice/MicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.kttk.products.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
